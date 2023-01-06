e75a1945b5bd4edce218f5c326d74d4c23093d60
rosaaliceknowles <120340173+rosaaliceknowles@users.noreply.github.com>
2023-01-06 11:19:37 -0500

    Update quadratic.js

--- quadratic.js
+++ quadratic.js
@@ -10,6 +10,17 @@ function quadratic(){
     answerArray[0] = ((-1*b)+sqrRoot)/(2*a); // (-b + (b^2 - 4ac)^1/2)/2a
     answerArray[1] = ((-1*b)-sqrRoot)/(2*a); // (-b - (b^2 - 4ac)^1/2)/2a
 
+    // changes the answers to have only 4 decimal places 
+    // does not change the answer to have 4 decimal places if it is a whole number (integer)
+
+    if (!Number.isInteger(answerArray[0])){
+        answerArray[0] = answerArray[0].toFixed(4);
+    }
+
+    if (!Number.isInteger(answerArray[1])){
+        answerArray[1] = answerArray[1].toFixed(4);
+    }
+
     document.getElementById("numbers").innerHTML = `Your inputs were: ${a}, ${b}, ${c}`;
 
     document.getElementById("answer").innerHTML = `The answer is: ${answerArray[0]}, ${answerArray[1]}`;
